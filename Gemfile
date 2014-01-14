d32ed97be2eed4c0048a7077016cc5f4b8f7590b
lengunry2013 <jiangqxa@yonyou.com>
2014-01-14 16:00:22 +0800

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem 'rubyzip',  "~> 0.9.9"
 
 gem'bootstrap-sass','2.3.2.0'
 
-gem 'bcrypt-ruby', '~> 3.1.2'
+gem 'bcrypt-ruby', '~>3.1.2'
 
 # Use sqlite3 as the database for Active Record
 #gem 'sqlite3'
@@ -20,6 +20,7 @@ end
 group :test do
 gem 'selenium-webdriver','2.0.0'
 gem 'capybara','2.1.0'
+gem 'factory_girl_rails','~>4.2.1'
 end
 
 # Use SCSS for stylesheets
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -64,6 +64,15 @@ describe User do
 
     it {should_not be_valid }
   end
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email){"User@ExAMple.Com"}
+    it "should be saved as all lower-case" do
+      @user.email=mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
 
   #验证密码和确认密码是否一致
   describe "when password is not present" do
