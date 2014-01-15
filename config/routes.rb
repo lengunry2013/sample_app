6c23ab71239f3911f63b70e05ec0dfe9429084dd
lengunry2013 <jiangqxa@yonyou.com>
2014-01-15 17:04:16 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 SampleApp::Application.routes.draw do
   resources :users
+  resources :sessions,only: [:new,:create, :destory]
   get "users/new"
   #get "static_pages/home"
   root to: 'static_pages#home'
   match '/signup',to: 'users#new', via: 'get'
+  match '/signin',to: 'sessions#new',via: 'get'
+  match'/signout',to: 'sessions#destroy', via: 'delete'
   #get "static_pages/help"
   match '/help', to: 'static_pages#help', via: 'get'
   #get "static_pages/about"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,7 @@ describe User do
   it {should respond_to(:password_digest)}
   it {should respond_to(:password)}
   it {should respond_to(:password_confirmation)}
+  it {should respond_to(:remember_token)}
   it {should respond_to(:authenticate)}
   it { should be_valid }
 
@@ -106,6 +107,11 @@ describe User do
        specify {expect(user_for_invalid_password).to be_false}
      end 
     
+  end
+  
+  describe "remember token" do
+    before {@user.save}
+    its(:remember_token) { should_not be_blank}
   end
 #pending "add some examples to (or delete) #{__FILE__}"
 end
